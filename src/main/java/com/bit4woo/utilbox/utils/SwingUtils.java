1b7cc775ac21d8bc7b2fbcef57d8c9d4d4e7e1c4
bit4woo <bit4woo@163.com>
2024-05-11 15:24:14 +0800

    Update SwingUtils.java

--- src/main/java/com/bit4woo/utilbox/utils/SwingUtils.java
+++ src/main/java/com/bit4woo/utilbox/utils/SwingUtils.java
@@ -11,20 +11,30 @@ public class SwingUtils {
 	public SwingUtils() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.print(showTextAreaDialog("aaaa"));
 	}
-	
-	
+
+
 	/**
 	 * 显示Text，让用户修改确认
 	 * @param text
 	 * @return
 	 */
 	public static String showTextAreaDialog(String text) {
+		return showTextAreaDialog(20, 20,"Edit And Confirm Text",text);
+	}
+
+
+	/**
+	 * 显示Text，让用户修改确认
+	 * @param text
+	 * @return
+	 */
+	public static String showTextAreaDialog(int rows, int columns,String title,String text) {
 		// 创建一个 JTextArea
-		JTextArea textArea = new JTextArea(10, 20); // 设置行数和列数
+		JTextArea textArea = new JTextArea(rows,columns); // 设置行数和列数
 		// 将 JTextArea 放入 JScrollPane 中，以便可以滚动查看
 		textArea.setText(text);
 		JScrollPane scrollPane = new JScrollPane(textArea);
@@ -32,7 +42,7 @@ public class SwingUtils {
 		int result = JOptionPane.showOptionDialog(
 				null, // parentComponent
 				scrollPane, // message
-				"Edit or Comfirm Text", // title
+				title, // title
 				JOptionPane.OK_CANCEL_OPTION, // optionType
 				JOptionPane.PLAIN_MESSAGE, // messageType
 				null, // icon
@@ -46,13 +56,13 @@ public class SwingUtils {
 		}
 		return null;
 	}
-	
-	
+
+
 	public static List<String> getLinesFromTextArea(JTextArea textarea){
 		return TextUtils.textToLines(textarea.getText());
 	}
-	
-	
+
+
 	public static List<String> getDeduplicatedLinesFromTextArea(JTextArea textarea){
 		return TextUtils.deduplicate(TextUtils.textToLines(textarea.getText()));
 	}
